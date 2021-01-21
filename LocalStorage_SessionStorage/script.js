fb5cea00255a46f98ad88a1276b412d714d80a8f
Muhammad Sohaib <msohaib@nisum.com>
2021-01-21 21:54:18 +0500

    Product Quantity or Price work is not complete

--- LocalStorage_SessionStorage/script.js
+++ LocalStorage_SessionStorage/script.js
@@ -1,84 +1,81 @@
+function Save() {
+  // Getting New Data From Button Click
+  var new_dataImg = document.getElementById("I");
+  new_dataImg.setAttribute("src", "C:/Users/msohaib/Desktop/images.jpg");
+  var new_dataDesc = document.getElementById("desc");
+  var new_dataPrice = document.getElementById("price");
+  // console.log(new_dataImg);
 
-// var price = document.getElementsByClassName(".price");
-// var i=0;
-var ProductList = document.getElementById("productsList");
+  var allData = {
+    myImage: new_dataImg.getAttribute("src"),
+    myDesc: new_dataDesc.innerHTML,
+    myPrice: new_dataPrice.innerHTML,
+  };
 
-function renderProduct1(){
-    var pImg = document.createElement("img");
-    pImg.setAttribute("src", "C:/Users/msohaib/Desktop/images.jpg");
-    pImg.setAttribute("width", "200px");
-    pImg.setAttribute("height", "200px");
-    pImg.setAttribute("alt", "Shampu");
-    ProductList.appendChild(pImg);
-   
-    // pushing Description
-    var desc = document.createElement("p");
-    var text = document.createTextNode("Sunsilk Shampu, This product is created by Pata Nahi");
-    desc.appendChild(text);
-    ProductList.appendChild(desc);
+  // TO save the data in Array when empty
+  if (localStorage.getItem("product") == null) {
+    localStorage.setItem("product", "[]");
+  }
+
+  // Old Data Swap with New Data
+  var old_data = JSON.parse(localStorage.getItem("product"));
+  old_data.push(allData);
+
+  // Save the old data with new data in local storage.
+  localStorage.setItem("product", JSON.stringify(old_data));
+}
+// document.onload = View();
+function View() {
+  // IF data is exeist then show in div.
+  if (localStorage.getItem("product") != null) {
     
-    //Pushing price 
-    var price = document.createElement("p");
-    var value = document.createTextNode("Rs.30000");
-    price.appendChild(value);
-    ProductList.appendChild(price);
+    var localValues = JSON.parse(localStorage.getItem("product"));
+    var data_div = document.getElementById("productsList");
 
-    var btn = document.createElement("button");
-    btn.setAttribute('onclick',"ClearLocalStorage();")
-    btn.innerHTML = "Cancel Item";
-    ProductList.appendChild(btn);
-    // console.log(pImg.getAttribute("src"))
-    // console.log(desc.innerHTML)
-    
-    var setTOLocal ={
-        Pimg: pImg.getAttribute("src"),
-        Desc:desc.innerHTML,
-        Price: price.innerHTML
+    // var h1Element = document.createElement('h1');
+      // var Node = document.createTextNode(JSON.parse(localStorage.getItem('product')));
+
+      // h1Element.appendChild(Node);
+      // data_div.appendChild(h1Element);
+    for (let i = 0; i < localValues.length; i++) {
+      
+      var pImg = document.createElement("img");
+
+      pImg.setAttribute("src", localValues[i].myImage);
+      data_div.appendChild(pImg);
+
+      var desc = document.createElement("p");
+      var text = document.createTextNode(localValues[i].myDesc);
+      desc.appendChild(text);
+      data_div.appendChild(desc);
+      // document.getElementById("d").innerHTML = localValues.Desc;
+
+      var price = document.createElement("p");
+      var pText = document.createTextNode(localValues[i].myPrice);
+      price.appendChild(pText);
+      data_div.appendChild(price);
     }
-   localStorage.setItem("image",JSON.stringify(setTOLocal));
+  }
+  else{
+    alert("Items in Product List is not added");
+}
 }
 
+function ItemQuantity(){
+    let count=0;
+    var qty = document.querySelectorAll('select');
+    console.log(qty);
 
-function renderProduct2(){
-    var pImg = document.createElement("img");
-    pImg.setAttribute("src", "C:/Users/msohaib/Desktop/maggi.png");
-    pImg.setAttribute("width", "200px");
-    pImg.setAttribute("height", "200px");
-    pImg.setAttribute("alt", "Shampu");
-    ProductList.appendChild(pImg);
-   
-    // pushing Description
-    var desc = document.createElement("p");
-    var text = document.createTextNode("Maggi maggi maggi");
-    desc.appendChild(text);
-    ProductList.appendChild(desc);
+    var localValues = JSON.parse(localStorage.getItem("product"));
+    // for(var i = 0; i < localStorage.product.length; i++){
+    //     console.log(i);
+    //   }
+
+    for(let i=0; i<localStorage.length; i++) {
+        let key = localStorage.key(i);
+        alert(`${key}: ${localStorage.key(key)}`);
+      }
     
-    //Pushing price 
-    var price = document.createElement("p");
-    var value = document.createTextNode("Rs.30000");
-    price.appendChild(value);
-    ProductList.appendChild(price);
 
-    var btn = document.createElement("button");
-    btn.setAttribute('onclick',"ClearLocalStorage();")
-    btn.innerHTML = "Cancel Item";
-    ProductList.appendChild(btn);
-
-    var setTOLocal ={
-        Pimg: pImg.getAttribute("src"),
-        Desc:desc.innerHTML,
-        Price: price.innerHTML
-    }
-    localStorage.setItem("image2",JSON.stringify(setTOLocal));
-   
-   
 }
-
-// Local Storage Data Show karwana hai......!
-var localData = document.getElementById("localData");
-let obj = JSON.parse(localStorage.getItem("image"));
-console.log(obj);
-var ima = document.createElement("div");
-ima.appendChild(obj);
-
-localData.appendChild(ima);
+window.onload = ItemQuantity()
